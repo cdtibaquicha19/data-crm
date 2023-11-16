a80740e565d3cd84c8d8fc42e86078aa498d8076
Cristyan Tibaquicha <cdtibaquicha19@gmail.com>
2023-11-16 13:56:47 -0500

    Carga inicial test 16/11/23

---
+++ resources/views/contenido.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.index')
+
+@section('contenido')
+    <!-- Contenido específico de esta página -->
+    <h1>Bienvenido a mi sitio</h1>
+    <p>Esta es la página de inicio.</p>
+
+
+    prueba 
+@endsection
